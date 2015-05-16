c924c91a11f22d3eaf1840675716fd170f1d1009
CarlSama <carlsama@163.com>
2015-05-16 21:30:53 +0800

    wrong for not think about UN-0

--- restore_ip_address.cpp
+++ restore_ip_address.cpp
@@ -9,7 +9,7 @@ class Solution {
 		vector<string> res;
 		string path;
 	public:
-		inline bool checkNumRange(string s,int &begin){
+		/*inline bool checkNumRange(string s,int &begin){
 			if(begin == s.size()) return false;
 			int num = 0;
 			while(s[begin] != '.'){
@@ -32,32 +32,40 @@ class Solution {
 				return lastNum > 0 && lastNum <= 255;
 			}	
 			return false;
-		}
-		void btrack(vector<string>& res,string& path,int dotNum,string& s,int currIdx){
-			if(dotNum == 3){
+		}*/
+		void btrack(vector<string>& res,string& path,int dotNum,string& s,int currIdx,int currNum){
+			if(currNum > 255)	return ;
+			if(dotNum == 3){//现在我们只需要检测最后一个
 				//将后面的全部放入path中
-				int len = path.size();
-				path += s.substr(currIdx,s.size() - currIdx);
-				if(isValidIpAddress(path))
+				int idx = currIdx;
+				while(s[idx]){
+					currNum *= 10;
+					currNum += s[idx] - '0';
+					++idx;
+				}
+				if(currNum <= 255){
+					int len = path.size();
+					path += s.substr(currIdx,s.size() - currIdx);
 					res.push_back(path);
-				path = path.substr(0,len);
+					path = path.substr(0,len);
+				}
 				return;
 			}
 			if(currIdx == s.size())
 				return ;
 
 			path.push_back('.');	path.push_back(s[currIdx]);
-			btrack(res,path,dotNum+1,s,currIdx+1);
+			btrack(res,path,dotNum+1,s,currIdx+1,s[currIdx]-'0');
 			path.pop_back();	path.pop_back();
 
 			path.push_back(s[currIdx]);
-			btrack(res,path,dotNum,s,currIdx+1);
+			btrack(res,path,dotNum,s,currIdx+1,(currNum * 10 + s[currIdx] - '0'));
 			path.pop_back();
 		}
 		vector<string> restoreIpAddresses(string s) {
-			if(s.size() < 4) return res;
+			if(s.size() < 4 || s.size() > 12) return res;
 			path.push_back(s[0]);//消除首部为'.'的情况
-			btrack(res,path,0,s,1);
+			btrack(res,path,0,s,1,s[0]-'0');
 			return res;
 		}
 };
