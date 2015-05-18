e41fc2feeaee590b506a081f1c6ab1359602005f
CarlSama <carlsama@163.com>
2015-05-18 14:37:33 +0800

    use memo to avoid TLE

--- unique-pathsII.cpp
+++ unique-pathsII.cpp
@@ -1,17 +1,21 @@
 #include<iostream>
 #include<vector>
+#include"print.h"
 
 using  namespace std;
 class Solution {
 	private:
 		int cols,rows;
 	public:
+		/*
+		 * let's use memo to avoid TLE
+		 * also, sliding arry to avoid MLE
+		 */
 		int resolve(vector<vector<int> >& grid,vector<vector<int> >&dp,int col,int row){
-			if(col == 0 || row == 0){
-				dp[col][row] = 1;
-				return 1;
-			}
+			if(dp[col][row] != -1)
+				return dp[col][row];
 
+			dp[col][row] = 0;
 			if(col - 1 >= 0 && !grid[col-1][row])
 				dp[col][row] += resolve(grid,dp,col-1,row);
 			if(row - 1 >= 0 && !grid[col][row-1])
@@ -21,15 +25,16 @@ class Solution {
 		}
 
 		int uniquePathsWithObstacles(vector<vector<int> >& obstacleGrid) {
-			if(obstacleGrid.size() == 0 || obstacleGrid[0].size() == 0) return 0;
+			if(obstacleGrid.size()==0 || obstacleGrid[0].size()==0)	return 0;
 
 			cols = obstacleGrid.size();
 			rows = obstacleGrid[0].size();
+			if(obstacleGrid[0][0] || obstacleGrid[cols-1][rows-1])	return 0;
 
-			vector<vector<int> > dp(cols,vector<int>(rows,0));
+			vector<vector<int> > dp(cols,vector<int>(rows,-1));
+			dp[0][0] = 1;
 
 			resolve(obstacleGrid,dp,cols-1,rows-1);
-
 			return dp[cols-1][rows-1];
 		}
 };
