ee8572f385dcc7e86974609e2441bc5059db3914
CarlSama <carlsama@163.com>
2015-06-29 12:47:24 +0800

    4-based

--- repeated-dna-sequences.cpp
+++ repeated-dna-sequences.cpp
@@ -1,30 +1,58 @@
 #include<iostream>
 #include<vector>
 #include<string>
-#include<unordered_map>
+#include<unordered_set>
 
 using  namespace std;
 
+/*
+ * Method 1 ：　使用unordered_map来标记所有长度为10的子串的重复性
+ *				MLE 
+ * Method 2 :   使用unordered_set　－》　TLE
+ *
+ * 考虑是因为string太长的原因，所以使用四进制存储
+ */
 class Solution {
 	const int subLen = 10;
 	public:
+		long charToLong(char c){
+			switch(c) {
+				case 'A':
+					return 0;
+				case 'C':
+					return 1;
+				case 'G':
+					return 2;
+				case 'T':
+					return 3;
+			}
+		}
+		long getValue(string str) {
+			long value = 0;
+			for(int i=str.size()-1 ; i>=0 ; --i) {
+				value *= 4;
+				value += charToLong(str[i]);
+			}
+			return value;
+		}
+
 		vector<string> findRepeatedDnaSequences(string s) {
 			vector<string> result;
-			//the bool means wether this string is added to result
-			unordered_map<string,bool> sequenceSeenTimes;
+			unordered_set<long> sequenceOnce;
+			unordered_set<long> sequenceMoreThanOnce;
 
 			int endIdx = s.size() - subLen + 1;
 			for(int i = 0 ; i < endIdx ; ++i) {
 				string subStr = s.substr(i,subLen);
-				auto iter = sequenceSeenTimes.find(subStr);
-				if(iter == sequenceSeenTimes.end())
-					sequenceSeenTimes.insert(pair<string,bool>(subStr,false));
-				else {
-					//发现重复
-					if(iter->second == false) {
-						//如果还未添加
+				long currValue = getValue(subStr);
+				if(sequenceMoreThanOnce.find(currValue) == sequenceMoreThanOnce.end()) {
+					auto iter = sequenceOnce.find(currValue);
+					if(iter == sequenceOnce.end()) {
+						sequenceOnce.insert(currValue);
+					}else{
 						result.push_back(subStr);
-						iter->second = true;
+						sequenceOnce.erase(iter);
+						sequenceMoreThanOnce.insert(currValue);
 					}
 				}
 			}
@@ -32,7 +60,7 @@ class Solution {
 		}
 };
 
-int main(){
+int main() {
 	string str = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
 	Solution s;
 	vector<string> vs = s.findRepeatedDnaSequences(str); 
